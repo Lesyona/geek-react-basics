5986b7039e01feaea9f94a978a9486eba985e891
Olesya Mirzakhmedova <olesya.mirzakhmedova@convergent.digital>
2021-09-21 17:44:18 +0300

    ДЗ к 5му уроку

--- lesson-05/src/App.js
+++ lesson-05/src/App.js
@@ -1,11 +1,13 @@
 import { Routes } from "./components/Routes";
 import React from "react";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 function App() {
   return (
-      <>
+      <Provider store={store}>
           <Routes />
-      </>
+      </Provider>
   );
 }
 
--- lesson-05/src/views/Profile/index.js
+++ lesson-05/src/views/Profile/index.js
@@ -1,9 +1,28 @@
 import {store} from "../../store";
+import Checkbox from '@material-ui/core/Checkbox';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import {toggleShowName} from "../../store/profile/actions";
+import {useSelector, useDispatch} from "react-redux";
 
 export default function Profile() {
-    const state = store.getState();
+    const showName = useSelector((state) => state.showName);
+    const dispatch = useDispatch();
 
-    console.log(state);
+    const clickCheckbox = () => {
+        dispatch(toggleShowName);
+    }
 
-    return <h1>Profile</h1>;
+    return (
+        <>
+            <h1>Profile</h1>
+            <FormGroup>
+                <FormControlLabel control={<Checkbox onClick={clickCheckbox} />} label="Show name" />
+            </FormGroup>
+
+            {showName &&
+                <p>User name: Vasya</p>
+            }
+        </>
+    );
 }
